f9445c45acc48d604f9d0bbab6d5f65365fe306f
gcc-shivansh <shivansh.glasscode@gmail.com>
2021-02-14 18:56:44 +0400

    Fixed stable 14ffeb2020

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,9 +19,24 @@
           $('.nav-menu .active, .mobile-nav .active').removeClass('active');
           $(this).closest('li').addClass('active');
         }
-
+        // if($('#header').hasClass('active'))
+        // {
+        //   $('#header').click(function (){
+        //     $('#movecontent').animate({
+        //       left:'700px'
+        //     })
+        //   })
+        // }
+        // else{
+        //   $('#header').click(function (){
+        //     $('#movecontent').animate({
+        //       right:'900px'
+        //     })
+        //   })
+        // }
         if (hash == '#header') {
-          $('#header').removeClass('header-top');
+          $('#header').removeClass('header-top ');
+          // $('#Homenav').addClass('homeHeader');
           $("section").removeClass('section-show');
           if ($('body').hasClass('mobile-nav-active')) {
             $('body').removeClass('mobile-nav-active');
@@ -33,10 +48,10 @@
 
         if (!$('#header').hasClass('header-top')) {
           $('#header').addClass('header-top');
+          // $('#Homenav').removeClass('homeHeader');
           setTimeout(function() {
             $("section").removeClass('section-show');
             $(hash).addClass('section-show');
-
           }, 350);
         } else {
           $("section").removeClass('section-show');
@@ -58,21 +73,6 @@
       }
     }
   });
-
-  // Activate/show sections on load with hash links
-  if (window.location.hash) {
-    var initial_nav = window.location.hash;
-    if ($(initial_nav).length) {
-      $('#header').addClass('header-top');
-      $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-      $('.nav-menu, .mobile-nav').find('a[href="' + initial_nav + '"]').parent('li').addClass('active');
-      setTimeout(function() {
-        $("section").removeClass('section-show');
-        $(initial_nav).addClass('section-show');
-      }, 350);
-    }
-  }
-
   // Mobile Navigation
   if ($('.nav-menu').length) {
     var $mobile_nav = $('.nav-menu').clone().prop({
@@ -102,12 +102,6 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
   // Skills section
   $('.skills-content').waypoint(function() {
     $('.progress .progress-bar').each(function() {
@@ -116,56 +110,4 @@
   }, {
     offset: '80%'
   });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
-
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
-  });
-
-  // Initiate venobox (lightbox feature used in portofilo)
-  $(document).ready(function() {
-    $('.venobox').venobox({
-      'share': false
-    });
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
 })(jQuery);
